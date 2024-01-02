43499ab73ac96c887ad2c84400f8a0160a84e8d6
JoshKeesee <105324950+JoshKeesee@users.noreply.github.com>
2024-01-02 03:16:53 +0000

    .

--- modules/getProject.js
+++ modules/getProject.js
@@ -1,34 +1,23 @@
 const defaultStats = require("./stats");
 const fs = require("fs");
-const projectsFolder = "../projects";
+const pf = "../projects";
+if (!fs.existsSync(pf)) fs.mkdirSync(pf);
 
 project = {
 	get(author, project) {
-		const p = `${projectsFolder}/${author}/${project}.json`;
-		if (!fs.existsSync(p)) return {
+		const p = `${pf}/${author}/${project}.json`;
+		/* if (!fs.existsSync(p)) */ return {
 			exists: false,
 			stats: defaultStats,
 		};
 		const data = JSON.parse(fs.readFileSync(p, "utf8"));
-		return {
-			exists: true,
-			map: data.map,
-			// {
-			// 	w: map.w,
-			// 	h: map.h,
-			// 	tsize: map.tsize,
-			// 	tilemap: map.tilemap,
-			// 	items: map.items,
-			// 	map: map.map,
-			// },
-			stats: data.stats || defaultStats,
-			...data,
-		};
+		data.exists = true;
+		return data;
 	},
 	make(author, project, data) {
-		const p = `${projectsFolder}/${author}/${project}.json`;
+		const p = `${pf}/${author}/${project}.json`;
 		if (fs.existsSync(p)) return false;
-		fs.mkdirSync(`${projectsFolder}/${author}`, { recursive: true });
+		fs.mkdirSync(`${pf}/${author}`, { recursive: true });
 		fs.writeFileSync(p, JSON.stringify(data);
 		return true;
 	},	
